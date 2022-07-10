db707a3e603e644f3f838581ffd2e28d53f85c8b
Adeel <54504931+madeelshabbir@users.noreply.github.com>
2022-07-10 23:29:26 +0500

    B-3: Configure shopify_app gem (#3)
    * .env.example: Added new example enviroment variables.
    * .rubocop.yml: Updated file configuration for ruby 2.1.3 and rails 7.0.3.
    * Gemfile: Added shopify_app gem.
    * Gemfile.lock: Auto-updated after installation of shopfiy_app gem.
    * app/controllers/authenticated_controller.rb: Auto-generated after installation of shopfiy_app gem.
    * app/controllers/home_controller.rb: Auto-generated after installation of shopfiy_app gem.
    * app/controllers/products_controller.rb: Auto-generated after installation of shopfiy_app gem.
    * app/javascript/lib/flash_messages.js: Auto-generated after installation of shopfiy_app gem.
    * app/javascript/lib/shopify_app.js: Auto-generated after installation of shopfiy_app gem.
    * app/jobs/app_uninstalled_job.rb: Created to receive app_uninstalled wehbook.
    * app/jobs/products_create_job.rb: Created to receive products_create wehbook.
    * app/jobs/products_delete_job.rb: Created to receive products_delete wehbook.
    * app/jobs/products_update_job.rb: Created to receive products_update wehbook.
    * app/jobs/shop_update_job.rb: Created to receive shop_update wehbook.
    * app/jobs/shopify_webhook_job.rb: Created as a base job for shopify webhook jobs.
    * app/models/shop.rb: Auto-generated after installation of shopfiy_app gem.
    * app/views/home/index.html.erb: Auto-generated after installation of shopfiy_app gem.
    * app/views/layouts/_flash_messages.html.erb: Auto-generated after installation of shopfiy_app gem.
    * app/views/layouts/embedded_app.html.erb: Auto-generated after installation of shopfiy_app gem.
    * config/database.yml: Auto-generated after installation of shopfiy_app gem.
    * config/environments/development.rb: Auto-updated after installation of shopfiy_app gem.
    * config/importmap.rb: Auto-updated after installation of shopfiy_app gem.
    * config/initializers/session_store.rb: Auto-generated after installation of shopfiy_app gem.
    * config/initializers/shopify_app.rb: Auto-generated after installation of shopfiy_app gem.
    * config/routes.rb: Auto-updated after installation of shopfiy_app gem.
    * db/migrate/20220709114319_create_shops.rb: Auto-generated after installation of shopfiy_app gem.
    * db/schema.rb: Auto-updated after running migration.

--- Gemfile
+++ Gemfile
@@ -79,3 +79,4 @@ group :test do
 end
 
 gem 'dotenv-rails'
+gem 'shopify_app', '~> 20.0'
---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+WEBHOOK_TOPICS = %w(shop/update app/uninstalled products/create products/update products/delete).freeze
+
+ShopifyApp.configure do |config|
+  config.application_name = ENV.fetch('SHOPIFY_APP_NAME')
+  config.old_secret = ''
+  config.scope = ENV.fetch('SHOPIFY_ACCESS_SCOPE')
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.api_version = ENV.fetch('SHOPIFY_API_VERSION')
+  config.shop_session_repository = 'Shop'
+  config.reauth_on_access_scope_changes = true
+  config.api_key = ENV.fetch('SHOPIFY_API_KEY')
+  config.secret = ENV.fetch('SHOPIFY_API_SECRET')
+  config.webhooks = WEBHOOK_TOPICS.map do |topic|
+    { topic:, address: "webhooks/#{topic.tr('/', '_')}", format: 'json' }
+  end
+end
+
+Rails.application.config.after_initialize do
+  if ShopifyApp.configuration.api_key.present? && ShopifyApp.configuration.secret.present?
+    ShopifyAPI::Context.setup(
+      api_key: ShopifyApp.configuration.api_key,
+      api_secret_key: ShopifyApp.configuration.secret,
+      api_version: ShopifyApp.configuration.api_version,
+      host_name: URI(ENV.fetch('SHOPIFY_APP_HOST', '')).host || '',
+      scope: ShopifyApp.configuration.scope,
+      is_private: !ENV.fetch('SHOPIFY_APP_PRIVATE_SHOP', '').empty?,
+      is_embedded: ShopifyApp.configuration.embedded_app,
+      session_storage: ShopifyApp::SessionRepository,
+      logger: Rails.logger,
+      private_shop: ENV.fetch('SHOPIFY_APP_PRIVATE_SHOP', nil),
+      user_agent_prefix: "ShopifyApp/#{ShopifyApp::VERSION}"
+    )
+
+    ShopifyApp::WebhooksManager.add_registrations
+  end
+end
