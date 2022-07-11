43f839ae45a62cfdb6aeff3160763066d30fe3f3
Adeel Shabbir <madeelshabbirs@gmail.com>
2022-07-11 23:57:55 +0500

    B-4: Configure sidekiq
    * .env.example: Added REDIS_URL sample environment variable.
    * Gemfile: Added sidekiq gem.
    * Gemfile.lock: Auto-updated due to sidekiq gem.
    * config/application.rb: Added sidekiq in queue adaptor configuration.
    * config/initializers/sidekiq.rb: Generated new file for sidekiq and redis confugurations.
    * config/routes.rb: Added mount for sidekiq dashboard.
    * config/sidekiq.yml: Added sidekiq queue configurations.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module AdviceShopify
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
