d5bca35c2d239c32d63a27cd6b6b291090cd5a83
Adeel <54504931+madeelshabbir@users.noreply.github.com>
2022-07-12 00:12:42 +0500

    B-4: Configure sidekiq (#4)
    * .env.example: Added REDIS_URL sample environment variable.
    * Gemfile: Added sidekiq gem.
    * Gemfile.lock: Auto-updated due to sidekiq gem.
    * config/application.rb: Added sidekiq in queue adaptor configuration.
    * config/initializers/sidekiq.rb: Generated new file for sidekiq and redis confugurations.
    * config/routes.rb: Added mount for sidekiq dashboard.
    * config/sidekiq.yml: Added sidekiq queue configurations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root to: 'home#index'
+
   resources :products, only: :index
+
   mount ShopifyApp::Engine, at: '/'
+  mount Sidekiq::Web, at: '/sidekiq'
 end
