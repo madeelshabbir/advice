95cfad931bd99386470a79a576c065ad60cfde58
Adeel <54504931+madeelshabbir@users.noreply.github.com>
2022-09-05 00:23:56 +0500

    B-11: Show customer wishlist (#11)
    * Gemfile: Added pagy, rack-cors, and also uncommented sassc-rails.
    * Gemfile.lock: Auto updated due to newly installed gems.
    * app/assets/images/loader.svg: Loader's animated svg file.
    * app/assets/stylesheets/application.css: Changed extension from css to scss and removed comments.
    * app/assets/stylesheets/application.scss: Added css generic class classes.
    * app/controllers/application_controller.rb: Included ApplicationHelper.
    * app/controllers/wishlists_controller.rb: Defined show action and implemented infinite scrolling.
    * app/helpers/application_helper.rb: Overrided pagy_url_for and added some other methods.
    * app/helpers/wishlists_helper.rb: Removed app_path method.
    * app/views/wishlists/_next_page.html.erb: Created partial for products next frame view.
    * app/views/wishlists/_product.html.erb: Created partial for product card.
    * app/views/wishlists/_wishlist.html.erb: Created partial for wishlist.
    * app/views/wishlists/scrollable_list.html.erb: Created view for products next frame.
    * app/views/wishlists/show.html.erb: Created view for show action for wishlist.
    * config/application.rb: Added assets host.
    * config/initializers/cors.rb: Defined rack cors policies.
    * config/initializers/pagy.rb: Required countless from pagy.
    * lib/shopify/asset.rb: Included ApplicationHelper.

--- Gemfile
+++ Gemfile
@@ -45,7 +45,7 @@ gem 'tzinfo-data', platforms: %i(mingw mswin x64_mingw jruby)
 gem 'bootsnap', require: false
 
 # Use Sass to process CSS
-# gem 'sassc-rails'
+gem 'sassc-rails'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem 'image_processing', '~> 1.2'
@@ -82,3 +82,5 @@ gem 'dotenv-rails'
 gem 'shopify_app', '~> 20.0'
 gem 'sidekiq'
 gem 'recursive-open-struct'
+gem 'pagy', '~> 5.10'
+gem 'rack-cors'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include Pagy::Frontend
+
+  def pagy_url_for(pagy, page, absolute: false, html_escaped: false)
+    path = url_for({ pagy.vars[:page_param] => page, only_path: !absolute })
+    "#{app_path}#{html_escaped ? path.gsub('&', '&amp;') : path}"
+  end
+
+  def svg(name)
+    file_path = Rails.root.join('app', 'assets', 'images', "#{name}.svg")
+    File.read(file_path).html_safe if File.exist?(file_path) # rubocop:disable Rails/OutputSafety
+  end
+
+  def app_path
+    "/apps/#{ENV.fetch('SHOPIFY_APP_HANDLE')}"
+  end
 end
