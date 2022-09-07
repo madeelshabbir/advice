f32f2c005cae2b9400a3a4d814fca790213abf07
Adeel <54504931+madeelshabbir@users.noreply.github.com>
2022-09-08 00:28:20 +0500

    B-12: Store variants information (#12)
    * app/jobs/products/base_job.rb: Build variants params.
    * app/jobs/products_create_job.rb: Created or updated variants.
    * app/jobs/products_update_job.rb: Created or updated variants.
    * app/models/customer.rb: Updated validations.
    * app/models/product.rb: Added has many association for variant and validations.
    * app/models/shop.rb: Added validations.
    * app/models/variant.rb: Created new model for variant.
    * db/migrate/20220905183851_create_variants.rb: Created new migration to create variants table.
    * db/schema.rb: Auto-updated due to the new migrations.
    * lib/builders/graphql/product.rb: Updated for variant payload.
    * lib/queries/graphql/product.rb: Updated to fetch variants fields.
    * lib/shopify/product.rb: Changed default limit for find_in_batch.

--- app/jobs/products/base_job.rb
+++ app/jobs/products/base_job.rb
@@ -2,6 +2,8 @@
 
 module Products
   class BaseJob < ShopifyWebhookJob
+    VARIANT_WHITELISTED_PARAMS = %i(title price compare_at_price).freeze
+
     protected
 
       def payload
@@ -11,6 +13,14 @@ module Products
           handle: params[:handle],
           featured_image: params.dig(:images, 0, :src)
         }
+      end
+
+      def create_or_update_variants(product)
+        params[:variants].each do |params|
+          product.variants
+                 .find_or_initialize_by(external_id: params[:id])
+                 .update!(**params.slice(*VARIANT_WHITELISTED_PARAMS))
+        end
       end
 
       def products
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,12 @@
 class Product < ApplicationRecord
   belongs_to :shop
 
+  has_many :variants, primary_key: :external_id, dependent: :destroy
   has_many :customer_products, primary_key: :external_id, dependent: :destroy
   has_many :customers, through: :customer_products
 
-  validates :external_id, :title, presence: true
+  accepts_nested_attributes_for :variants
+
+  validates :external_id, :title, :handle, presence: true
+  validates :external_id, uniqueness: true
 end
